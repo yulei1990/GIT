58352ad1f73d9de2c8ee38dfeccedc0e330028d0
yulei1990 <you@326060431@qq.com>
2015-03-22 11:24:49 +0800

    a code that prints hello world

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: hello.cpp
+	> Author: Comst
+	> Mail:750145240@qq.com 
+	> Created Time: Sun 22 Mar 2015 11:23:15 AM CST
+ ************************************************************************/
+
+#include<iostream>
+int
+main( void )
+{
+	std::cout << "hello world" << std::endl;
+
+	return 0;
+}
